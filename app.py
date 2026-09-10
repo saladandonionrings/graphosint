024f0fb046b29838083a8f6bee3626c7355ddd41
salad <salad>
2026-09-10 15:40:45 +0200

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="GraphOSINT | Intelligence Link Analysis",
+    page_title="GraphOSINT",
     page_icon="🕸️",
     layout="wide",
     initial_sidebar_state="collapsed"
